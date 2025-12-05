c2253a7c3eb2b3c3e7461a5b4b8dfd4e6d4622b9
will <will29982@gmail.com>
2025-12-05 01:00:47 +0000

    feat: 添加 Vite 服务器代理配置以支持 API 请求

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/accounts': 'http://localhost:8000',
+      '/market': 'http://localhost:8000',
+      '/orders': 'http://localhost:8000',
+    }
+  }
 })
